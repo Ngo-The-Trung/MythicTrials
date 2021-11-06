3859508e55d843945a5333ca54b68cd5c4ab9582
Ngo The Trung <ngo.the.trung.aczne@gmail.com>
2021-11-06 19:47:16 +0000

    Danse Macabre impl

--- ExampleMod.py
+++ ExampleMod.py
@@ -1,85 +1,148 @@
 # Add the base directory to sys.path for testing- allows us to run the mod directly for quick testing
+import ctypes
+import inspect
 import sys
 sys.path.append('../..')
 
+
 import CommonContent
 import Consumables
+# import Game
 import Level
-import Monsters
-import Spells
-import Game
+# import Monsters
+import Mutators
 
 
-def add_tod_factory(unit_factory):
-    def unit_with_tod():
-        # Add touch of death to unit
-        unit = unit_factory()
+class AllEnemiesToD(Mutators.Mutator):
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = "All enemies learn Touch of Death"
+        self.global_triggers[Level.EventOnUnitPreAdded] = self.on_unit_pre_added
+
+    def on_unit_pre_added(self, evt):
+        self.add_tod(evt.unit)
+
+    def on_levelgen(self, levelgen):
+        for unit in levelgen.level.units:
+            self.add_tod(unit)
+
+    def add_tod(self, unit):
+        if unit.is_lair:
+            return
+
+        if unit.team == Level.TEAM_PLAYER:
+            return
+
+        # Remove base melee attack
+        for i, spell in enumerate(unit.spells):
+            if isinstance(spell, CommonContent.SimpleMeleeAttack):
+                unit.spells.pop(i)
+                break
+
+        # Each unit needs its own spell instance btw
         tod = CommonContent.SimpleMeleeAttack(damage=200, damage_type=Level.Tags.Dark)
         tod.name = "Touch of Death"
+        unit.spells.insert(0, tod)
 
-        # TODO remove regular attacks, cleaner this way
-        unit.spells.insert(0, tod)  # preferred to regular attacks
+        # From Level.py:3024
+        # Wouldn't surprise me if future versions crashed due to this
+        unit.spells[0].caster = unit
+        unit.spells[0].owner = unit
 
         return unit
 
-    return unit_with_tod
+
+class AllConsumablesDeathDice(Mutators.Mutator):
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = "A non health/mana potion consumables are replaced with Death Dice"
+
+    def on_levelgen(self, levelgen):
+        self.replace_consumables(levelgen.items)
+
+    def replace_consumables(self, items):
+        for i, item in enumerate(items):
+            if item.name != "Healing Potion" and item.name != "Mana Potion":
+                items[i] = Consumables.death_dice()
 
 
-def add_tod_all_units():
-    for i, (unit, level) in enumerate(Monsters.spawn_options):
-        Monsters.spawn_options[i] = (add_tod_factory(unit), level)
+class MordredOnlyWeakness(Mutators.Mutator):
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = "Mordred can only be damaged through Death Dice"
+
+        self.global_triggers[Level.EventOnPreDamaged] = self.on_pre_damaged
+
+    def on_pre_damaged(self, evt):
+        if evt.unit.name != "Mordred":
+            return
+
+        if isinstance(evt.source, Consumables.DeathDiceSpell):
+            return
+
+        # Reduces damage to 0
+        for f in inspect.stack()[::-1]:
+            if f.function != "deal_damage":
+                continue
+
+            frame = f.frame
+            frame.f_locals.update({
+                "amount": 0,
+            })
+            # clown world
+            ctypes.pythonapi.PyFrame_LocalsToFast(ctypes.py_object(frame), ctypes.c_int(0))
+
+##### Debugging/cheating
+
+class BigCheat(Mutators.Mutator):
+    def __init__(self):
+        Mutators.Mutator.__init__(self)
+        self.description = "Testing"
+        self.global_triggers[Level.EventOnUnitPreAdded] = self.on_unit_pre_added
+
+    def on_game_begin(self, game):
+        game.p1.max_hp = 10000
+        game.p1.cur_hp = 10000
+        for tag in [
+            Level.Tags.Dark,
+            Level.Tags.Arcane,
+            Level.Tags.Physical,
+            Level.Tags.Fire,
+            Level.Tags.Ice,
+            Level.Tags.Lightning,
+            Level.Tags.Poison,
+        ]:
+            game.p1.resists[tag] = 50
+
+        # Death dices
+        for _ in range(50):
+            game.p1.add_item(Consumables.death_dice())
+
+    def on_unit_pre_added(self, evt):
+        self.make_mordred_dumb(evt.unit)
+
+    def on_levelgen(self, levelgen):
+        for unit in levelgen.level.units:
+            self.make_mordred_dumb(unit)
+
+    def make_mordred_dumb(self, unit):
+        if unit.name != "Mordred":
+            return
+
+        unit.spells = []
+
+        return unit
+
+##### End Debugging
 
 
-def only_death_dice():
-    Consumables.all_consumables = [
-        (Consumables.death_dice, Consumables.COMMON)
-    ]
+Mutators.all_trials.append(
+    Mutators.Trial("Danse Macabre", [
+        AllEnemiesToD(),
+        AllConsumablesDeathDice(),
+        MordredOnlyWeakness(),
 
+        BigCheat(),
+    ])
+)
 
-def mordred_only_weakness():
-    # Make Mordred take 0 damage from any spell that's not Death Dice
-    orig_deal_damage = Level.Level.deal_damage
-
-    def deal_damage(self, x, y, amount, damage_type, source, flash=True):
-        unit = self.get_unit_at(x, y)
-        if isinstance(unit, Monsters.Mordred):
-            print("[MOD] Attempting to hurt Mordred with %s" % (source))
-        return orig_deal_damage(self, x, y, amount, damage_type, source, flash=flash)
-
-    Level.Level.deal_damage = deal_damage
-
-
-add_tod_all_units()
-only_death_dice()
-mordred_only_weakness()
-
-
-def test():
-    def make_test_character():
-        orig_make_player_character = Game.Game.make_player_character
-        def make_invincible_char(self):
-            player = orig_make_player_character(self)
-            player.max_hp = 500
-            player.cur_hp = 500
-            for tag in [Level.Tags.Dark, Level.Tags.Physical, Level.Tags.Fire, Level.Tags.Ice, Level.Tags.Lightning]:
-                player.resists[tag] = 200
-
-            for _ in range(20):
-                player.add_item(Consumables.death_dice())
-
-            return player
-
-        # Make something invincible
-        Game.Game.make_player_character = make_invincible_char
-
-    def spawn_mordred():
-        Monsters.spawn_options = [
-            (Monsters.Goblin, 1),
-            (Monsters.Mordred, 1),
-        ]
-
-    make_test_character()
-    spawn_mordred()
-
-
-test()
